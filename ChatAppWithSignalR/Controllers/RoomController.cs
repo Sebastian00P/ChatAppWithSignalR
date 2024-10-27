ba42d44d2289423670f92f04fea340f6ce7cd37d
Sebastian00P <w64182@student.wsiz.edu.pl>
2024-10-27 13:38:01 +0100

    Custom register and login pages

---
+++ ChatAppWithSignalR/Controllers/RoomController.cs
@@ -0,0 +1,47 @@
+﻿using ChatAppWithSignalR.Services.RoomServices;
+using ChatAppWithSignalR.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatAppWithSignalR.Controllers
+{
+    [Authorize]
+    public class RoomController : Controller
+    {
+        private readonly IRoomService _roomService;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public RoomController(IRoomService roomService, UserManager<IdentityUser> userManager)
+        {
+            _roomService = roomService;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var model = await _roomService.GetAllAsync();
+            return View(model);
+        }
+
+        public async Task<IActionResult> CreateRoom(string chatName)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var owner = await _userManager.GetUserAsync(User);
+            var room = new Room
+            {
+                OwnerId = owner.Id,
+                Id = Guid.NewGuid(),
+                MessageIds = new List<Guid>(),
+                UserIds = new List<string>() { owner.Id },
+                ChatName = chatName
+            };
+            await _roomService.CreateAsync(room);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ChatAppWithSignalR/Hub/ChatHub.cs
@@ -0,0 +1,44 @@
+﻿using ChatAppWithSignalR.Services.MessageServices;
+using ChatAppWithSignalR.ViewModels;
+using Microsoft.AspNetCore.SignalR;
+using System.Text.RegularExpressions;
+
+namespace ChatAppWithSignalR.Hub
+{
+    public class ChatHub : Microsoft.AspNetCore.SignalR.Hub
+    {
+        private readonly IMessageService messageService;
+
+        public ChatHub(IMessageService messageService)
+        {
+            this.messageService = messageService;
+        }
+
+        public async Task JoinGroup(string roomId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+        }
+        public async Task SendMessage(string chatRoomId, string userId, string messageContent)
+        {
+            Message message = new()
+            {
+                Id = Guid.NewGuid(),
+                MessageContent = messageContent,
+                UserId = userId,
+                CreationTime = DateTime.Now,
+                IsActive = true,
+                ChatRoomId = Guid.Parse(chatRoomId)
+            };
+
+            await messageService.CreateMessageAsync(message);
+
+            await Clients.Group(chatRoomId).SendAsync(HubMethods.ReceiveMessage, userId, messageContent);
+        }
+        public string GetConnectionId() => Context.ConnectionId;
+
+        public static class HubMethods
+        {
+            public static string ReceiveMessage = nameof(ReceiveMessage);
+        }
+    }
+}
