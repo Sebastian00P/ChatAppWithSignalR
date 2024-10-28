332d01508ac8e1bf81964a7775295df8fd18f3a6
Sebastian00P <w64182@student.wsiz.edu.pl>
2024-10-28 17:21:35 +0100

    Add roomName to viewmodel

--- ChatAppWithSignalR/ViewModels/RoomViewModel.cs
+++ ChatAppWithSignalR/ViewModels/RoomViewModel.cs
@@ -6,6 +6,7 @@ namespace ChatAppWithSignalR.ViewModels
     public class RoomViewModel
     {
         public string RoomId { get; set; }
+        public string RoomName { get; set; }
         public List<Message> Messages { get; set; }
         public ClaimsPrincipal User { get; set; }
     }
